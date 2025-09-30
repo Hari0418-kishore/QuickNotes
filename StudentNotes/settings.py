16f41fbeba6bbf4559005e8797dc8652926b000a
HariKishore <harikishoretm@gmail.com>
2025-09-30 12:10:43 +0530

    Adding STATIC_ROOT for render deployment

--- StudentNotes/settings.py
+++ StudentNotes/settings.py
@@ -35,9 +35,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2ijt31f+e38+0i^d4h@-dkt!hefge^1xii@-z6=b(#0zb7n1*+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["your-app-name.onrender.com"]
 
 
 # Application definition
@@ -135,11 +135,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# Required for Render deployment even if no static files are used
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
